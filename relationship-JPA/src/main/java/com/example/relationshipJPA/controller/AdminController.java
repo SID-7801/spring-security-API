f20a94538e3d8e1b4e9c962b4019866eacfe2539
Dixitvara <varadixit@gmail.com>
2024-04-01 18:28:06 +0530

    admin view not approved users api done,

--- relationship-JPA/src/main/java/com/example/relationshipJPA/controller/AdminController.java
+++ relationship-JPA/src/main/java/com/example/relationshipJPA/controller/AdminController.java
@@ -35,7 +35,6 @@ public class AdminController {
             return Utils.getResponseEntity("Internal server error!", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-
     // view approvals api for admin
     @GetMapping("/view-requests")
     public List<Member> viewNotApprovedUsers()
